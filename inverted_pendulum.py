4519930c8714800bd3d2577112252fd47aead98a
Vahid <vsaberi.iust@gmail.com>
2018-01-01 16:42:38 -0800

    first commit!

--- inverted_pendulum.py
+++ inverted_pendulum.py
@@ -1,3 +1,31 @@
 import gym
 
 
+env=gym.make('CartPole-v0')         #inverted pendulum environment
+
+print(env.reset())         #reset env to random state
+
+# These are the sates: [cart position, cart velocity, pole angle, pole tip velocity]
+
+box=env.observation_space           #state space
+
+print(box.low)          #min value of possible states
+print(box.high)         #max value of possible states
+print(box.sample())     #a sample of state space
+
+actions=env.action_space        #action space
+#
+print(actions.n)   #number of actions in action space
+
+
+#random step!
+done=False
+k=0
+while not done:
+    observation, reward, done, info =env.step(actions.sample())
+    k=k+1
+    print(observation)
+    print(reward)
+    print(done)
+    print(info)
+    print(k)
