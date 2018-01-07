b3508605e3b1df237e97a8fcd63ad0dd1bde76d6
Vahid <vsaberi.iust@gmail.com>
2018-01-07 01:51:10 -0800

    mountain cart q learning using RBF finished

--- Mountain_car_q_learning.py
+++ Mountain_car_q_learning.py
@@ -11,6 +11,7 @@ from sklearn.pipeline import FeatureUnion
 from sklearn.preprocessing import StandardScaler
 from sklearn.kernel_approximation import RBFSampler
 from sklearn.linear_model import SGDRegressor
+from mpl_toolkits.mplot3d import Axes3D
 
 
 #SGDRegressor defaults:
@@ -72,7 +73,7 @@ class Model:
 
         #create a Q model for each action (using SGDRegression)
         for i in range(env.action_space.n):
-            model=SGDRegressor(learning_rate=learning_rate)
+            model=SGDRegressor(learning_rate=learning_rate,eta0=0.001)
 
             #the initial target is zero
             #the target is inside [] (since the default is 2D)
@@ -89,7 +90,7 @@ class Model:
 
 
     def update(self,s,a,G):
-        s_feature=self.feature_transformer([s])
+        s_feature=self.feature_transformer.transform([s])
         assert(len(s_feature.shape)==2)
 
         #[G] to make it 2D
@@ -110,20 +111,95 @@ def play_episode(env,model,eps,gamma):
     totalreward=0
     t=0
 
-    while not done and iter<10000:
+    while not done and t<10000:
         action=model.greedy_policy(s,eps)
         s_previous=s
         s,reward,done,info=env.step(action)
 
+        # if done and t<199:
+        #     reward=1
         #update the model
         G=reward+gamma*np.max(model.predict(s)[0])
+        # print("G:",model.predict(s)[0])
         model.update(s_previous,action,G)
 
         totalreward+=reward
         t+=1
+        # print("s:",s)
 
-        return totalreward
+    return totalreward
 
 
-    
 
+def plot_cost_to_go(env,estimator,num_tiles=20):
+    x=np.linspace(env.observation_space.low[0],env.observation_space.high[0],num_tiles)
+    y = np.linspace(env.observation_space.low[1], env.observation_space.high[1], num_tiles)
+
+    X,Y=np.meshgrid(x,y)
+
+    Z=np.apply_along_axis(lambda x: -np.max(estimator.predict(x)),2,np.dstack((x,y)))
+
+    fig=plt.figure(figsize=(10,5))
+
+    # ax=fig.add_subplot(111,projection='3d')
+    ax=Axes3D(fig)
+    surf=ax.plot_surface(X,Y,Z,
+                         rstride=1, cstride=1, cmap=matplotlib.cm.coolwarm, vmin=1.0,vmax=1.0)
+    ax.set_xlabel('position')
+    ax.set_ylabel('velocity')
+    ax.set_zlabel('Cost-to-go=-V(s)')
+    ax.set_title('Cost-to-go_Function')
+
+    fig.colorbar(surf)
+
+    plt.show()
+
+
+def plot_running_average(totalrewards):
+    N=len(totalrewards)
+    running_ave=np.empty(N)
+
+    for t in range(N):
+        running_ave[t]=totalrewards[max(0,t-100):(t+1)].mean()
+    plt.plot(running_ave)
+    plt.title("Running Average")
+    plt.show()
+
+
+if __name__=='__main__':
+    env=gym.make('MountainCar-v0')
+    ft=FeatureTransformer(env)
+    model=Model(env,ft,"constant")
+
+    gamma=0.99
+
+    if 'monitor' in sys.argv:
+
+        filename=os.path.basename(__file__).split('.')[0]
+        monitor_dir='./'+filename+'_'+str(datetime.now())
+        env=wrappers.Monitor(env,monitor_dir)
+
+    N = 1500
+
+    totalrewards = np.empty(N)
+
+    for n in range(N):
+        eps = 0.1 *(0.97**(n/10))
+        # eps=0.01
+        totalreward = play_episode(env, model, eps, gamma)
+        totalrewards[n] = totalreward
+
+        print("episode:", n, "total reward:", totalreward, "eps:", eps)
+        print("avg reward for last 100 episodes:", totalrewards[max(0, n - 100):(n + 1)].mean())
+
+
+    print("avg reward for last 100 episodes:", 100*totalrewards[-100:].mean())
+
+    print("total steps:", -totalrewards.sum())
+
+    plt.plot(totalrewards)
+    plt.title("Rewards")
+    plt.show()
+    plot_running_average(totalrewards)
+
+    plot_cost_to_go(env,model)
